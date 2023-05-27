949cdcac31ff9a274b3f1645a3a1a93e7dba723d
Jin Su Cho <blacksky9795@naver.com>
2023-05-27 17:58:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 13508 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+    val a = sc.nextLong()
+    val b = sc.nextLong()
+    val c = sc.nextLong()
+    println(a+b+c)
+}
