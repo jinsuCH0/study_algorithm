4001c7ba185a45310f1425e32795c67bbb62e9e5
Jin Su Cho <blacksky9795@naver.com>
2023-05-27 18:03:20 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 124 ms, Memory: 13516 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
@@ -0,0 +1,7 @@
+import java.util.Scanner
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+    val n = sc.nextLong()
+    for (i in 1..n/4) { print("long ") }
+    println("int")
+}
